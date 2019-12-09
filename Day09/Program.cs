b2d8198f850490cd570b7dcd6444fb623cbe60b1
Gerardufoin <gerardufoin2@hotmail.com>
2019-12-09 19:57:45 +0100

    Day09
    Ninth day of Advent of Code 2019

---
+++ Day09/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntcodeComputer computer = new IntcodeComputer(Advent2019.Utils.GetInput(args));
+            List<BigInteger> output = computer.Execute(1);
+            Console.WriteLine("Part 1: " + output[0]);
+            output = computer.Execute(2);
+            Console.WriteLine("Part 2: " + output[0]);
+        }
+    }
+}
