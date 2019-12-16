42759a22503040ca8bd23df8625a5eafaa8ec574
Gerardufoin <gerardufoin2@hotmail.com>
2019-12-16 21:40:36 +0100

    Day16
    Sixteenth day of Advent of Code 2019

---
+++ Day16/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Day16
+{
+    class Program
+    {
+        static List<int> GetSignal(string input)
+        {
+            return Regex.Matches(input, @"\d{1}").Cast<Match>().Select(c => int.Parse(c.Value)).ToList();
+        }
+
+        #region Part 1
+        static List<int> ApplyPattern(List<int> signal, int[] pattern)
+        {
+            List<int> newPattern = new List<int>();
+            for (int i = 1; i < signal.Count + 1; ++i)
+            {
+                newPattern.Add(Math.Abs(signal.Select((s, idx) => s * pattern[(idx + 1) / i % pattern.Length]).Sum() % 10));
+            }
+            return newPattern;
+        }
+
+        static List<int> FlawedFrequencyTransmission(List<int> signal, int[] pattern, int phases)
+        {
+            for (int i = 0; i < phases; ++i)
+            {
+                signal = ApplyPattern(signal, pattern);
+            }
+            return signal;
+        }
+
+        static List<int> RepeatSignal(List<int> signal, int n)
+        {
+            List<int> ret = new List<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                ret.AddRange(signal);
+            }
+            return ret;
+        }
+        #endregion
+
+        #region Part 2 (slow ~20sec)
+        static List<int> ApplyPatternForP2(List<int> signal)
+        {
+            LinkedList<int> newPattern = new LinkedList<int>();
+            for (int i = signal.Count - 1; i >= 0; --i)
+            {
+                if (newPattern.Count == 0)
+                {
+                    newPattern.AddFirst(signal[i]);
+                }
+                else
+                {
+                    newPattern.AddFirst((newPattern.First() + signal[i]) % 10);
+                }
+            }
+            return new List<int>(newPattern);
+        }
+
+        static List<int> FFTWithOffset(List<int> signal, int phases, int offset)
+        {
+            // Because our pattern begins with 0, all numbers before the offset will be 0 once we reach the offset
+            signal.RemoveRange(0, offset);
+            for (int i = 0; i < phases; ++i)
+            {
+                signal = ApplyPatternForP2(signal);
+            }
+            return signal;
+        }
+        #endregion
+
+        #region Math Part 2 (failed to simplify the formula, unusable as it is, longer than the previous method)
+        static long DeepPhibonacci(int n, int depth, Dictionary<(int num, int depth), int> memory)
+        {
+            if (depth < 1)
+            {
+                return 1;
+            }
+            long val = 0;
+            Stack<(int n, int d)> result = new Stack<(int n, int d)>();
+            result.Push((n, depth - 1));
+            while (result.Count > 0)
+            {
+                Console.WriteLine(result.Count);
+                (int n, int d) first = result.First();
+                result.Pop();
+                if (first.d > 0)
+                {
+                    for (int i = 1; i <= (first.d == depth - 1 ? first.n - 1 : first.n); ++i)
+                    {
+                        result.Push((i, first.d - 1));
+                    }
+                }
+                val += first.n;
+            }
+            return val;
+        }
+
+        static List<long> CreateMultiTable(int phases, int signalLength)
+        {
+            List<long> table = new List<long>();
+            Dictionary<(int num, int depth), int> memory = new Dictionary<(int num, int depth), int>();
+            for (int i = 0; i < signalLength; ++i)
+            {
+                table.Add(DeepPhibonacci(phases, i, memory));
+            }
+            return table;
+        }
+
+        static int OptiFFTWithOffset(List<int> signal, int phases, int offset)
+        {
+            // Because our pattern begins with 0, all numbers before the offset will be 0 once we reach the offset
+            signal.RemoveRange(0, offset);
+            List<long> test = CreateMultiTable(4, 10);// signal.Count);
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.WriteLine(test[i]);
+            }
+            return 0;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            List<int> signal = GetSignal(Advent2019.Utils.GetInput(args));
+            int[] pattern = new int[] { 0, 1, 0, -1 };
+            Console.WriteLine("Part 1: " + string.Join("", FlawedFrequencyTransmission(signal, pattern, 100).Take(8).Select(s => Math.Abs(s).ToString()).ToArray()));
+            int offset = (int)signal.Take(7).Select((s, i) => s * Math.Pow(10, 6 - i)).Sum();
+            Console.WriteLine("Part 2: " + string.Join("", FFTWithOffset(RepeatSignal(signal, 10000), 100, offset).Take(8).Select(s => Math.Abs(s).ToString()).ToArray()));
+        }
+    }
+}
